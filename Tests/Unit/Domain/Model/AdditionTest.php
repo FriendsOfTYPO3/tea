bb52944347059c332fa1852402be012a19dff172
mostwanted <roland.waldner@gmail.com>
2014-01-02 00:00:34 +0100

    Add @return void annotation to test methods

--- Tests/Unit/Domain/Model/AdditionTest.php
+++ Tests/Unit/Domain/Model/AdditionTest.php
@@ -46,6 +46,7 @@ class AdditionTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
 
 	/**
 	 * @test
+	 * @return void
 	 */
 	public function getTitleInitiallyReturnsEmptyString() {
 		$this->assertSame(
@@ -56,6 +57,7 @@ class AdditionTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
 
 	/**
 	 * @test
+	 * @return void
 	 */
 	public function setTitleSetsTitle() {
 		$this->subject->setTitle('foo bar');
