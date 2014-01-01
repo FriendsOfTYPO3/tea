64303b35b228fcbfca7ebb9a66d1759808ce427e
mostwanted <roland.waldner@gmail.com>
2014-01-02 00:03:30 +0100

    Add @return void annotation to test methods

--- Tests/Unit/Domain/Repository/TestimonialRepositoryTest.php
+++ Tests/Unit/Domain/Repository/TestimonialRepositoryTest.php
@@ -53,6 +53,7 @@ class TestimonialRepositoryTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCa
 
 	/**
 	 * @test
+	 * @return void
 	 */
 	public function canBeInstantiated() {
 		$this->assertNotNull(
