3b383423452788c4f00dd98f37272ce56bdb5d2c
Oliver Klee <typo3-coding@oliverklee.de>
2017-04-24 17:43:59 +0200

    [TASK] Require TYPO3 >= 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,10 +30,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.6.99',
-            'typo3' => '6.2.0-7.6.99',
-            'extbase' => '6.2.0-7.6.99',
-            'fluid' => '6.2.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
             'phpunit' => '4.0.0-5.9.99',
         ],
         'conflicts' => [
