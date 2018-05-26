a4089e04e953610222f217768eee34f08847e79c
Oliver Klee <github@oliverklee.de>
2018-05-27 00:28:13 +0200

    [FEATURE] Functional tests for the tea controller (#21)

---
+++ Tests/Functional/Controller/TeaControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+namespace OliverKlee\Tea\Tests\Functional\Controller;
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+
+/**
+ * Test case.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class TeaControllerTest extends FunctionalTestCase
+{
+    /**
+     * @var string[]
+     */
+    protected $testExtensionsToLoad = ['typo3conf/ext/tea'];
+
+    /**
+     * @var string[]
+     */
+    protected $coreExtensionsToLoad = ['fluid_styled_content'];
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->importDataSet(__DIR__ . '/Fixtures/Database/pages.xml');
+        $this->importDataSet(__DIR__ . '/../Domain/Repository/Fixtures/Product/Tea.xml');
+
+        $this->setUpFrontendRootPage(
+            1,
+            [
+                'EXT:fluid_styled_content/Configuration/TypoScript/setup.txt',
+                'EXT:tea/Configuration/TypoScript/setup.txt',
+                'EXT:tea/Tests/Functional/Controller/Fixtures/Frontend/Basic.typoscript',
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function indexActionRendersTeaTitle()
+    {
+        $responseContent = $this->getFrontendResponse(1)->getContent();
+
+        $teaTitle = 'Earl Grey';
+        $this->assertContains($teaTitle, $responseContent);
+    }
+}
