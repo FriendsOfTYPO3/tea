792f665524f33f3cbf70529129495d6c477e1bb6
Oliver Klee <github@oliverklee.de>
2018-11-05 23:31:59 +0100

    [CLEANUP] Streamline ext_emconf.php (#37)

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,18 +2,20 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Tea example',
     'description' => 'Example extension for unit testing and best practices',
-    'category' => 'example',
-    'author' => 'Oliver Klee',
-    'author_email' => 'typo3-coding@oliverklee.de',
-    'author_company' => 'oliverklee.de',
-    'state' => 'stable',
     'version' => '2.0.x-dev',
+    'category' => 'example',
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-7.2.99',
             'typo3' => '7.6.0-8.7.99',
         ],
     ],
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'author' => 'Oliver Klee',
+    'author_email' => 'typo3-coding@oliverklee.de',
+    'author_company' => 'oliverklee.de',
     'autoload' => [
         'psr-4' => [
             'OliverKlee\\Tea\\' => 'Classes/',
