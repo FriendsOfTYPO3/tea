f85488f5f533c1147820e656f579304fcd6e2f1f
Łukasz Uznański <l.uznanski@macopedia.com>
2020-07-07 03:20:03 +0200

    [TASK] Remove zip occurencies

--- .gitlab/ci/docker_install.sh
+++ .gitlab/ci/docker_install.sh
@@ -5,7 +5,7 @@
 set -xe
 
 apt-get update -yqq
-apt-get install git libzip -yqq
+apt-get install git -yqq
 
 php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
 chmod +x /usr/local/bin/composer
@@ -13,4 +13,4 @@ chmod +x /usr/local/bin/composer
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
-docker-php-ext-install pdo_mysql zip
+docker-php-ext-install pdo_mysql
