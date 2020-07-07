92cf748b6595f49100dc043efb2689fd5a672912
Łukasz Uznański <l.uznanski@macopedia.com>
2020-07-07 03:05:36 +0200

    [TASK] Add before script with composer install

---
+++ .gitlab/ci/docker_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+apt-get update -yqq
+apt-get install git -yqq
+
+php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
+chmod +x /usr/local/bin/composer
+
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+docker-php-ext-install pdo_mysql
