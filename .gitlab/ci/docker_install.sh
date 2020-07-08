76883a6b9c3f7d67ea2510eedce05117c466f8d2
Łukasz Uznański <l.uznanski@macopedia.com>
2020-07-08 17:58:43 +0200

    [TASK] Add parallel package

--- .gitlab/ci/docker_install.sh
+++ .gitlab/ci/docker_install.sh
@@ -5,7 +5,7 @@
 set -xe
 
 apt-get update -yqq
-apt-get install git libzip-dev unzip -yqq
+apt-get install git libzip-dev unzip parallel -yqq
 
 php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
 chmod +x /usr/local/bin/composer
