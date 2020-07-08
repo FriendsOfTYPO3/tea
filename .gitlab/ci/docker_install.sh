8b180d8bcaa856dc5d443b54beaf6b0757e4e9a4
Łukasz Uznański <l.uznanski@macopedia.com>
2020-07-08 02:55:07 +0200

    [TASK] Add unzip

--- .gitlab/ci/docker_install.sh
+++ .gitlab/ci/docker_install.sh
@@ -5,7 +5,7 @@
 set -xe
 
 apt-get update -yqq
-apt-get install git libzip-dev -yqq
+apt-get install git libzip-dev unzip -yqq
 
 php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
 chmod +x /usr/local/bin/composer
