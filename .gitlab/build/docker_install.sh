f276169fb89e9a6a478584ae4e6242e590bb10bd
Łukasz Uznański <l.uznanski@macopedia.pl>
2020-07-19 23:55:00 +0200

    [TASK] Update gitlab ci

--- .gitlab/build/docker_install.sh
+++ .gitlab/build/docker_install.sh
@@ -13,4 +13,4 @@ chmod +x /usr/local/bin/composer
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
-docker-php-ext-install pdo_mysql zip
+docker-php-ext-install pdo_mysql zip mysql
