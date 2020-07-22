d05850678140e5ece89ee59feb2caef7ca75973f
Łukasz Uznański <l.uznanski@macopedia.pl>
2020-07-23 00:58:26 +0200

    [TASK] Update frontend setup

--- Resources/Public/TypeScript/Test.ts
+++ Resources/Public/TypeScript/Test.ts
@@ -1,19 +1,23 @@
 class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
-    }
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
 }
 
 interface Person {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
 function greeter(person: Person) {
-    return 'Hello, ' + person.firstName + ' ' + person.lastName;
+  return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-const user = new Student('Jane', 'M.', 'User');
+const user = new Student("Jane", "M.", "User");
 
 document.body.textContent = greeter(user);
