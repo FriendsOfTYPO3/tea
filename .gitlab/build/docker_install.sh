4ba9f80ffe93740f9e86f9cb9304a536a4e9d7a6
Łukasz Uznański <l.uznanski@macopedia.pl>
2020-07-24 15:31:47 +0200

    [TASK] Update yamllint install

--- .gitlab/build/docker_install.sh
+++ .gitlab/build/docker_install.sh
@@ -6,7 +6,7 @@ set -xe
 
 apt-get update -yqq
 apt-get install git libzip-dev unzip libxml2-utils python-pip -yqq
-pip install yamllint==1.10.0 -yqq
+pip install yamllint==1.24.2
 
 php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
 chmod +x /usr/local/bin/composer
