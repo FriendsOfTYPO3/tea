ad30fa7619a464e5a0c5c8d6782d3e319bbe53bb
Łukasz Uznański <l.uznanski@macopedia.com>
2020-08-05 12:31:49 +0200

    [TASK] Format js file

--- Resources/Public/JavaScript/Test.js
+++ Resources/Public/JavaScript/Test.js
@@ -1,20 +1,20 @@
 const menuConfig = {
-  title: "Order",
-  buttonText: "Send",
-  cancellable: true,
+    title: "Order",
+    buttonText: "Send",
+    cancellable: true,
 };
 
 function createMenu(config) {
-  config = Object.assign(
-    {
-      title: "Foo",
-      body: "Bar",
-      buttonText: "Baz",
-      cancellable: true,
-    },
-    config
-  );
-  return config;
+    config = Object.assign(
+        {
+            title: "Foo",
+            body: "Bar",
+            buttonText: "Baz",
+            cancellable: true,
+        },
+        config
+    );
+    return config;
 }
 
 createMenu(menuConfig);
