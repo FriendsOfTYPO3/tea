a61af514063685934deefc1224552edda6e6aa51
Łukasz Uznański <l.uznanski@macopedia.com>
2020-09-02 01:53:35 +0200

    [TASK] Remove not needed line

--- .gitlab/build/docker_install.sh
+++ .gitlab/build/docker_install.sh
@@ -7,8 +7,6 @@ set -xe
 apt-get update -yqq
 apt-get install git libzip-dev unzip libxml2-utils wget -yqq
 
-rm -rf /var/lib/apt/lists/*
-
 php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
 chmod +x /usr/local/bin/composer
 
