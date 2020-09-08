cd036fbe7063e626f251632958bd18b355126062
Łukasz Uznański <l.uznanski@macopedia.com>
2020-09-08 03:14:25 +0200

    [TASK] Add test stage

--- .gitlab/build/docker_install.sh
+++ .gitlab/build/docker_install.sh
@@ -5,9 +5,12 @@
 set -xe
 
 apt-get update -yqq
-apt-get install git libzip-dev unzip libxml2-utils wget -yqq
+apt-get install git libzip-dev unzip parallel libxml2-utils wget -yqq
 
 php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer
 chmod +x /usr/local/bin/composer
 
-docker-php-ext-install zip
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+docker-php-ext-install pdo_mysql zip mysqli
