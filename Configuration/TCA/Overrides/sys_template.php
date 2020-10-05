0cba92276a9f7e78ec6243a44890a1d6cd208aeb
Łukasz Uznański <l.uznanski@macopedia.com>
2020-10-05 02:29:48 +0200

    [TASK] Add styles and js

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,3 +2,5 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript', 'Tea');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea',  'Configuration/TypoScript/Frontend/', 'Tea frontend (optional)'
+);
