e197210f2a8ba4cc6e9f731f715adeb7f447e054
Łukasz Uznański <lukaszuznanski94@gmail.com>
2020-10-14 11:06:07 +0200

    [TASK] Sniff and codestyle fix to php in configuration dir (#122)
    Co-authored-by: Łukasz Uznański <l.uznanski@macopedia.com>

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die('Access denied.');
+
+defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript', 'Tea');
