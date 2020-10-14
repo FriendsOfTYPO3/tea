2e726b2d8c63d5723263a3f15ca69f4c7822bd06
Łukasz Uznański <l.uznanski@macopedia.com>
2020-10-14 15:51:09 +0200

    [TASK] Fix codestyle

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,6 +1,6 @@
 <?php
-defined('TYPO3_MODE') or die('Access denied.');
+
+defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript', 'Tea');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea',  'Configuration/TypoScript/Frontend/', 'Tea frontend (optional)'
-);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript/Frontend/', 'Tea frontend (optional)');
