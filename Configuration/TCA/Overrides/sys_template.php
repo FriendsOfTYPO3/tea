d4ec80de29401bc37e61ae7ef24839596f015777
Łukasz Uznański <lukaszuznanski94@gmail.com>
2020-10-31 15:04:44 +0100

    [FEATURE] Add frontend linting (#120)
    Co-authored-by: Łukasz Uznański <l.uznanski@macopedia.com>
    Co-authored-by: Oliver Klee <typo3-coding@oliverklee.de>

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,3 +3,4 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript', 'Tea');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript/Frontend/', 'Tea frontend (optional)');
