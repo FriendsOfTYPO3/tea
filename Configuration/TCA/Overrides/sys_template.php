e1dccb045e122dd6a29edc875e8f457a76e82d81
Oliver Klee <typo3-coding@oliverklee.de>
2020-11-29 15:11:56 +0100

    [CLEANUP] Autoformat all files
    This will reduce unrelated autoformatting changes in other commits
    (and it also makes the code more readable).

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,4 +3,8 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript', 'Tea');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tea', 'Configuration/TypoScript/Frontend/', 'Tea frontend (optional)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'tea',
+    'Configuration/TypoScript/Frontend/',
+    'Tea frontend (optional)'
+);
