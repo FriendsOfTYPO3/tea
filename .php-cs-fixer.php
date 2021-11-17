5af57b2274a899de6ff9fbcda6d72cf3204e4b96
Łukasz Uznański <l.uznanski@macopedia.com>
2021-11-17 16:03:51 +0100

    Add more directories

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,5 @@
 <?php
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()->in('Classes', 'Configuration', 'Tests');
+$config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
 return $config;
