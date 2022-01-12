34eaf0cab1cdf4cf74f1e34e20faaff9e1c85bf3
Oliver Klee <typo3-coding@oliverklee.de>
2022-01-12 11:22:59 +0100

    [TASK] Drop support for TYPO3 9LTS (#363)
    Now that we have added support for TYPO3 11LTS, we have been supporting
    3 different TYPO3 LTS versions in the same branch. This has turned out
    to be too much of a hassle when running the test. So now is the time
    to drop support for 9LTS.

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-7.4.99',
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
     ],
     'state' => 'stable',
