68d0e226624a86f39939ad0e84e9bcc3ef8d3405
Oliver Klee <typo3-coding@oliverklee.de>
2022-03-29 15:32:40 +0200

    [CLEANUP] Add generics annotation to the Tea repository
    This helps static code analysis determine for which models the
    repository is responsible.

--- Classes/Domain/Repository/Product/TeaRepository.php
+++ Classes/Domain/Repository/Product/TeaRepository.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace TTN\Tea\Domain\Repository\Product;
 
+use TTN\Tea\Domain\Model\Product\Tea;
 use TTN\Tea\Domain\Repository\Traits\StoragePageAgnosticTrait;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 /**
- * Repository for Tea models.
+ * @extends Repository<Tea>
  */
 class TeaRepository extends Repository
 {
