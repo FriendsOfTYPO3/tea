bd6a6a1b3df6426d8166f59873fa8f432fe49573
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2022-04-29 14:05:43 +0200

    [TASK] re-lint js file to adapt to current changes

--- Resources/Public/JavaScript/Main.js
+++ Resources/Public/JavaScript/Main.js
@@ -2,35 +2,35 @@ var TYPO3 = TYPO3 || {};
 TYPO3.tea = {};
 
 TYPO3.tea.makeSortable = function (table) {
-    var th = table.tHead,
-        i;
-    th && (th = th.rows[0]) && (th = th.cells);
-    if (th) i = th.length;
-    else return;
-    while (--i >= 0)
-        (function (i) {
-            var dir = 1;
-            th[i].addEventListener('click', function () {
-                TYPO3.tea.sortTable(table, i, (dir = 1 - dir));
-            });
-        })(i);
+  var th = table.tHead,
+    i;
+  th && (th = th.rows[0]) && (th = th.cells);
+  if (th) i = th.length;
+  else return;
+  while (--i >= 0)
+    (function (i) {
+      var dir = 1;
+      th[i].addEventListener('click', function () {
+        TYPO3.tea.sortTable(table, i, (dir = 1 - dir));
+      });
+    })(i);
 };
 
 TYPO3.tea.sortTable = function (table, col, reverse) {
-    var tb = table.tBodies[0],
-        tr = Array.prototype.slice.call(tb.rows, 0),
-        i;
-    reverse = -(+reverse || -1);
-    tr = tr.sort(function (a, b) {
-        return reverse * a.cells[col].textContent.trim().localeCompare(b.cells[col].textContent.trim());
-    });
-    for (i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
+  var tb = table.tBodies[0],
+    tr = Array.prototype.slice.call(tb.rows, 0),
+    i;
+  reverse = -(+reverse || -1);
+  tr = tr.sort(function (a, b) {
+    return reverse * a.cells[col].textContent.trim().localeCompare(b.cells[col].textContent.trim());
+  });
+  for (i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    var t = document.querySelectorAll('.tx-tea table'),
-        i = t.length;
-    while (--i >= 0) {
-        TYPO3.tea.makeSortable(t[i]);
-    }
+  var t = document.querySelectorAll('.tx-tea table'),
+    i = t.length;
+  while (--i >= 0) {
+    TYPO3.tea.makeSortable(t[i]);
+  }
 });
