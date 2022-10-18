0e6d9dbd30dfd4fbee356b06a92f37e08f9aa716
Łukasz Uznański <l.uznanski@macopedia.com>
2022-10-18 21:14:28 +0200

    Stop injecting query settings

--- Classes/Domain/Repository/Traits/StoragePageAgnosticTrait.php
+++ Classes/Domain/Repository/Traits/StoragePageAgnosticTrait.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace TTN\Tea\Domain\Repository\Traits;
 
-use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 
 /**
  * This trait for repositories makes the repository ignore the storage page setting when fetching models.
  */
 trait StoragePageAgnosticTrait
 {
-    private QuerySettingsInterface $querySettings;
-
-    public function injectQuerySettings(QuerySettingsInterface $querySettings): void
-    {
-        $this->querySettings = $querySettings;
-    }
-
     public function initializeObject(): void
     {
-        $querySettings = clone $this->querySettings;
-
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
         $querySettings->setRespectStoragePage(false);
         $this->setDefaultQuerySettings($querySettings);
     }
