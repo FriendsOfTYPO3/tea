40d4225a06a4668964641cfe48ddf1ac3610c23a
Łukasz Uznański <l.uznanski@macopedia.com>
2023-02-21 23:17:08 +0100

    Make records localizable

--- Configuration/TCA/tx_tea_domain_model_product_tea.php
+++ Configuration/TCA/tx_tea_domain_model_product_tea.php
@@ -13,6 +13,10 @@ $tca = [
         'enablecolumns' => [
             'fe_group' => 'fe_group',
         ],
+        'transOrigPointerField' => 'l18n_parent',
+        'transOrigDiffSourceField' => 'l18n_diffsource',
+        'languageField' => 'sys_language_uid',
+        'translationSource' => 'l10n_source',
     ],
     'types' => [
         '1' => ['showitem' => '
@@ -29,6 +33,43 @@ $tca = [
         ],
     ],
     'columns' => [
+        'sys_language_uid' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'language',
+            ],
+        ],
+        'l18n_parent' => [
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0,
+                    ],
+                ],
+                'foreign_table' => 'tx_myextension_domain_model_something',
+                'foreign_table_where' =>
+                    'AND {#tx_myextension_domain_model_something}.{#pid}=###CURRENT_PID###'
+                    . ' AND {#tx_myextension_domain_model_something}.{#sys_language_uid} IN (-1,0)',
+                'default' => 0,
+            ],
+        ],
+        'l10n_source' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
+        'l18n_diffsource' => [
+            'config' => [
+                'type' => 'passthrough',
+                'default' => '',
+            ],
+        ],
         'title' => [
             'label' => 'LLL:EXT:tea/Resources/Private/Language/locallang_db.xlf:tx_tea_domain_model_product_tea.title',
             'config' => [
