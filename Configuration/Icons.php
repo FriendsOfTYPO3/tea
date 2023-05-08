4df4165df8cb8b9580384c4491600f82be05ccf9
lina.wolf <lwolf@w-commerce.de>
2023-05-08 16:38:12 +0000

    [FEATURE] Display Plugins in "New Content Element" Wizard
    resolves https://github.com/TYPO3-Documentation/tea/issues/825

---
+++ Configuration/Icons.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // Icon identifier
+    'tea_plugin' => [
+        // Icon provider class
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        // The source SVG for the SvgIconProvider
+        'source' => 'EXT:tea/Resources/Public/Icons/Plugin.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use TTN\Tea\Controller\TeaController;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die('Access denied.');
@@ -32,3 +35,10 @@ ExtensionUtility::configurePlugin(
         TeaController::class => '',
     ]
 );
+$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+if ($versionInformation->getMajorVersion() < 12) {
+    ExtensionManagementUtility::addPageTSConfig(
+        '@import "EXT:tea/Configuration/page.tsconfig"'
+    );
+}
