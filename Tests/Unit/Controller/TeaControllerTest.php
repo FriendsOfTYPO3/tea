286aa05073155a9e511627a974292ad987f6c4dd
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-01 21:15:42 +0100

    [TASK] Use more stubs in the unit tests (#962)
    This way, the purpose of the stub/mock is more explicit.
    
    Fixes #870

--- Tests/Unit/Controller/TeaControllerTest.php
+++ Tests/Unit/Controller/TeaControllerTest.php
@@ -51,8 +51,8 @@ final class TeaControllerTest extends UnitTestCase
         $this->viewMock = $this->createMock(TemplateView::class);
         $this->subject->_set('view', $this->viewMock);
 
-        $responseMock = $this->createMock(HtmlResponse::class);
-        $this->subject->method('htmlResponse')->willReturn($responseMock);
+        $responseStub = $this->createStub(HtmlResponse::class);
+        $this->subject->method('htmlResponse')->willReturn($responseStub);
     }
 
     /**
