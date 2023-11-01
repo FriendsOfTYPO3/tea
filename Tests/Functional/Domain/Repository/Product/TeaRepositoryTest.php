b4741eff26f6aaef98cb2662f5a5e3d403e9211a
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-01 15:41:50 +0100

    [CLEANUP] Use power of the DI container in the functional tests

--- Tests/Functional/Domain/Repository/Product/TeaRepositoryTest.php
+++ Tests/Functional/Domain/Repository/Product/TeaRepositoryTest.php
@@ -6,9 +6,8 @@ namespace TTN\Tea\Tests\Functional\Domain\Repository\Product;
 
 use TTN\Tea\Domain\Model\Product\Tea;
 use TTN\Tea\Domain\Repository\Product\TeaRepository;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
-use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
+use TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
@@ -21,15 +20,15 @@ final class TeaRepositoryTest extends FunctionalTestCase
 
     private TeaRepository $subject;
 
-    private PersistenceManager $persistenceManager;
+    private PersistenceManagerInterface $persistenceManager;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->persistenceManager = GeneralUtility::makeInstance(PersistenceManager::class);
+        $this->persistenceManager = $this->get(PersistenceManagerInterface::class);
 
-        $this->subject = $this->getContainer()->get(TeaRepository::class);
+        $this->subject = $this->get(TeaRepository::class);
     }
 
     /**
