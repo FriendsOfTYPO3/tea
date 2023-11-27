6691c8b737c40226535a00f12945c93bbf279fda
Eike Starkmann <eike.starkmann@posteo.de>
2023-11-27 16:22:32 +0100

    [TASK] remove trait and respect storage pid
    Ref:#1024

--- Classes/Domain/Repository/Product/TeaRepository.php
+++ Classes/Domain/Repository/Product/TeaRepository.php
@@ -6,6 +6,8 @@ namespace TTN\Tea\Domain\Repository\Product;
 
 use TTN\Tea\Domain\Model\Product\Tea;
 use TTN\Tea\Domain\Repository\Traits\StoragePageAgnosticTrait;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
@@ -25,6 +27,9 @@ class TeaRepository extends Repository
     public function findByOwnerUid(int $ownerUid): QueryResultInterface
     {
         $query = $this->createQuery();
+        $querySettings = $query->getQuerySettings();
+        $querySettings->setRespectStoragePage(false);
+        $query->setQuerySettings($querySettings);
         $query->matching($query->equals('ownerUid', $ownerUid));
 
         return $query->execute();
