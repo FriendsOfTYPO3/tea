f4f0ee316fdf6b942d5140b4d93914a039399433
Oliver Klee <typo3-coding@oliverklee.de>
2024-05-06 12:21:57 +0200

    [TASK] Improve the type annotations for `TeaRepository`
    This fixes the last remaining PHPStan level 6 warning.

--- Classes/Domain/Repository/TeaRepository.php
+++ Classes/Domain/Repository/TeaRepository.php
@@ -18,6 +18,8 @@ class TeaRepository extends Repository
 
     /**
      * @param positive-int $ownerUid
+     *
+     * @return QueryResultInterface<Tea>
      */
     public function findByOwnerUid(int $ownerUid): QueryResultInterface
     {
