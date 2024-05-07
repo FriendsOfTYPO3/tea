e86634a510db4c0677b2df9baaad4bfd43b4877b
Oliver Klee <typo3-coding@oliverklee.de>
2024-05-07 16:52:34 +0200

    [TASK] Drop obsolete options from `ext_emconf.php`

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,8 +15,6 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'stable',
-    'uploadfolder' => false,
-    'createDirs' => '',
     'author' => 'Oliver Klee, Daniel Siepmann, Łukasz Uznański',
     'author_email' => 'typo3-coding@oliverklee.de, coding@daniel-siepmann.de, lukaszuznanski94@gmail.com',
     'author_company' => 'TYPO3 Best Practices Team',
