e98388fce3406dd117aa1aa9fb59a276a266be27
Łukasz Uznański <l.uznanski@macopedia.com>
2024-05-07 13:28:06 +0200

    Add imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       ecmaVersion: 2022,
