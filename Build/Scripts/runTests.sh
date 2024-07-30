185535be65d28b55c10211547d0c01064c0d4781
Eike Starkmann <eike.starkmann@undkonsorten.com>
2024-07-30 18:36:56 +0200

    [BUGFIX] remove debug echo
    Related: #1399

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -561,7 +561,6 @@ case ${TEST_SUITE} in
             sqlite)
                 CONTAINERPARAMS="-e typo3DatabaseDriver=pdo_sqlite"
                 ${CONTAINER_BIN} run ${CONTAINER_COMMON_PARAMS} --name functional-${SUFFIX} ${XDEBUG_MODE} -e XDEBUG_CONFIG="${XDEBUG_CONFIG}" ${CONTAINERPARAMS} ${IMAGE_PHP} ${COMMAND}
-                echo "${CONTAINER_BIN} run ${CONTAINER_COMMON_PARAMS} --name functional-${SUFFIX} ${XDEBUG_MODE} -e XDEBUG_CONFIG=\"${XDEBUG_CONFIG}\" ${CONTAINERPARAMS} ${IMAGE_PHP} ${COMMAND}"
                 SUITE_EXIT_CODE=$?
                 ;;
         esac
