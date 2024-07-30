77f9b5c04b87bcc4edad9ba172bdef8f5acb9e05
Karsten Nowak <nowak@undkonsorten.com>
2024-07-30 20:41:05 +0200

    [TASK] Remove unused imports
    Related #1120

--- Classes/Command/CreateTestDataCommand.php
+++ Classes/Command/CreateTestDataCommand.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 namespace TTN\Tea\Command;
 
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use TYPO3\CMS\Backend\Command\ProgressListener\ReferenceIndexProgressListener;
-use TYPO3\CMS\Backend\Command\ReferenceIndexUpdateCommand;
-use TYPO3\CMS\Core\Core\Bootstrap;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\ReferenceIndex;
